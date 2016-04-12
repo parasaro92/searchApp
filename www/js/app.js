fc309e8ee23b01e3e0f0bf9eab8fbc820ff6576d
paras <rockypro92@gmail.com>
2016-04-12 14:44:55 +0530

    Added sliding favorities and refreshing of list

--- www/js/app.js
+++ www/js/app.js
@@ -33,6 +33,18 @@ angular.module('starter', ['ionic'])
       $scope.artists.splice($scope.artists.indexOf(item), 1);
     }
 
+    $scope.doRefresh = function(){
+
+      $http.get('js/data.json').success(function(data){
+        $scope.artists = data.artists;
+        $scope.$broadcast('scroll.refreshComplete');
+      });
+    }
+
+    $scope.toggleStar = function(item){
+      item.star = !item.star;
+    }
+
     $scope.moveItem = function(item, fromIndex, toIndex){
 
       $scope.artists.splice(fromIndex, 1);
